11e505d28207303addce17ec39f2a061d426335d
vishalkmar <vk722413@gmail.com>
2025-08-30 20:13:49 +0530

    configure db url atals to databse

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/jobportalDb');
+        await mongoose.connect('mongodb+srv://vishalkumar:VishalKumar@jobportal.a2xtkdd.mongodb.net/?retryWrites=true&w=majority&appName=jobportal');
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
