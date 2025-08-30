5dac8e09940e8feaf4ea0fc7ce2b296143427a9a
vishalkmar <vk722413@gmail.com>
2025-08-30 20:44:29 +0530

    cors fixed

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportalxxx.netlify.app/',
     credentials:true
 }
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 
 const PORT = 6000;
 
