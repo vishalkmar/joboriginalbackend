a3b8edd5c822767d8099b4590b660c5327114db1
vishalkmar <vk722413@gmail.com>
2025-08-30 21:18:29 +0530

    fixed cors again

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobportalxxx.netlify.app/',
+    origin:'https://jobportalxxx.netlify.app',
     credentials:true
 }
 
